39698321fd59a4a4608d244cc9f82bc23fc9fac0
Linux Developer for android <linux_dentalpockets@comsec.co.in>
2020-04-23 09:37:12 +0530

    Step 2 Complete.

--- lib/main.dart
+++ lib/main.dart
@@ -5,3 +5,17 @@ void main() => runApp(
         home: null,
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: Text('Ask Me Anything'),
+      ),
+      body: Container(),
+      backgroundColor: Colors.blue,
+    );
+  }
+}
